112398fe3f511186d5f81f80e2eb264698d3f89d
Aksafer <165661771+Aksafer@users.noreply.github.com>
2024-05-29 05:09:30 +0300

    Update بايو.py

--- MatrixMusic/plugins/play/بايو.py
+++ MatrixMusic/plugins/play/بايو.py
@@ -9,7 +9,7 @@ from pyrogram.types import (InlineKeyboardButton,CallbackQuery,InlineKeyboardMar
 from MatrixMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 
 
-@app.on_message(filters.command(["بايوهات مزخرفه","اسماء مزخرفه","زخارف"], ""))
+@app.on_message(filters.command(["بايوهات مزخرفه","اسماء مزخرفه","زغرفه"], ""))
 async def abrag(c: Client, m: Message):
     global mid
     mid = m.id
